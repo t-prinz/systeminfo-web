c179f8fabbfc2ddff4724831b9283c4cfeb8aea9
Trey Prinz <tprinz@redhat.com>
2019-09-26 16:44:24 -0500

    still testing

--- wsgi.py
+++ wsgi.py
@@ -6,16 +6,18 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
   web_con = ""
+
+#  web_con += pre_write()
   return getinfo(web_con)
 
 ########################################
 
 def getinfo(local_con):
-  local_con = "<h1>This is a header</h1>\n"
-  local_con += "Hello World from function!\n"
+#  local_con = "<h1>This is a header</h1>\n"
+#  local_con += "Hello World from function!\n"
 
-#  local_con = """<h1>This is a header</h1>\n
-#Hello World from function!\n"""
+  local_con = """<h1>This is a header</h1>\n
+Hello World from function!\n"""
 
   return local_con
 
