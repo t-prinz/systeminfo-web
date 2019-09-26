49ab8dcfbeda2316eae63b13cccb216646578e82
Trey Prinz <tprinz@redhat.com>
2019-09-26 16:30:56 -0500

    still testing

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,22 @@
 from flask import Flask
 application = Flask(__name__)
 
+########################################
+
 @application.route("/")
 def hello():
-    return getinfo()
-#    return "Hello World from Trey!"
+  web_con = ""
+  return getinfo(web_con)
 
-def getinfo():
-  web_con = "<h1>This is a header</h1>\n"
-  web_con += "Hello World from function!\n"
+########################################
 
-  return web_con
+def getinfo(local_con):
+  local_con = "<h1>This is a header</h1>\n"
+  local_con += "Hello World from function!\n"
+
+  return local_con
+
+########################################
 
 if __name__ == "__main__":
     application.run()
