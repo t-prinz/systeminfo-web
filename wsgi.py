4a0a087588d0cfbf4dc88fb8a79fb94c8987205e
Trey Prinz <tprinz@redhat.com>
2019-09-26 16:03:07 -0500

    still testing

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ def hello():
 #    return "Hello World from Trey!"
 
 def getinfo():
-  web_con = "<title>Hello from Flask</title>\n"
+  web_con = "<h1>Hello from Flask</h1>\n"
   web_con += "Hello World from function!\n"
   return web_con
 
