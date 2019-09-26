3f1de1c6de41479745fd48025ce978083f5c41c8
Trey Prinz <tprinz@redhat.com>
2019-09-26 13:07:10 -0500

    debugging

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,10 @@ def hello():
 #    return "Hello World from Trey!"
 
 def getinfo():
-  print("Hello World from function!")
+  print("Content-type:text/html\r\n\r\n")
+  print("<!DOCTYPE html>\n")
+  print("<h1>Hello World from function!</h1>\n")
+  print("</html>\n")
 
 if __name__ == "__main__":
     application.run()
