90002f4c076f3b6c392b2ed49b1bfefc528ece25
Trey Prinz <tprinz@redhat.com>
2019-09-30 07:57:32 -0500

    testing

--- wsgi.py
+++ wsgi.py
@@ -27,9 +27,9 @@ def pre_write():
 
   <style>
 
-  body {
-      background-color: powderblue;
-  }
+#  body {
+#      background-color: powderblue;
+#  }
 
   table {
       font-family: arial, sans-serif;
